d73bf546190c15c1c3c27358152c56db470b1d90
William Hicklin <venturehicklin@gmail.com>
2023-07-31 13:17:31 +0100

    Added #ifdef to enable ModeBase interaction with OnOff cluster only if the OnOff cluster is enabled.

--- src/app/clusters/mode-base-server/mode-base-server.cpp
+++ src/app/clusters/mode-base-server/mode-base-server.cpp
@@ -194,6 +194,7 @@ CHIP_ERROR Instance::Init()
         }
     }
 
+#ifdef EMBER_AF_PLUGIN_ON_OFF
     // OnMode with Power Up
     // If the On/Off feature is supported and the On/Off cluster attribute StartUpOnOff is present, with a
     // value of On (turn on at power up), then the CurrentMode attribute SHALL be set to the OnMode attribute
@@ -227,6 +228,7 @@ CHIP_ERROR Instance::Init()
             }
         }
     }
+#endif
 
     return CHIP_NO_ERROR;
 }
